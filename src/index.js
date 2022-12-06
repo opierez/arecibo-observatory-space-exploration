26e1487673b035db6c3fd7976bdab29cc1f42168
Jandro <71239294+coolcoolcoolyo@users.noreply.github.com>
2022-12-06 11:13:35 -0500

    Jandro commit change two

--- src/index.js
+++ src/index.js
@@ -2,9 +2,34 @@
 const form = document.querySelector('#search-form')
 const featuredImageDiv = document.querySelector('#featured_image')
 const mainImage = document.querySelector('#main_image')
+const mainTitle = document.querySelector('#featured-title')
+const mainExplain = document.querySelector('#featured-explain')
 
 // Event Listeners
 form.addEventListener('submit', handleSubmit)
+
+// https://api.nasa.gov/ --> scroll down to APOD ('Astronomy Pic Of the Day'), same as HEROKU's. Generates random image of the day. Jandro 12/5/22 evening
+fetch('https://api.nasa.gov/planetary/apod?api_key=XjBC4L80dCM7UOyBSupD3sYfCDTWcuAN5LIeb1dv')
+    .then(resp => resp.json())
+    .then(apod => {
+        // console.log(apod)
+        // console.log(apod.title)
+        // console.log(apod.explanation)
+        // console.log(apod.url)
+        renderAPOD(apod)
+    })
+
+function renderAPOD(apod) {
+    let apodTitle = apod.title
+    let apodImage = apod.url
+    let apodExplain = apod.explanation
+    
+    mainTitle.textContent = apodTitle
+    mainImage.src = apodImage
+    mainExplain.textContent = apodExplain
+    
+}
+
 
 // Allows the user to input a search term in the form and then fetch related images from NASA API
 function handleSubmit(e) {
@@ -18,7 +43,6 @@ function handleSubmit(e) {
         // console.log(data)
         // console.log(data.collection.items)
         getRandomImage(data)
-
     })
 }
 
@@ -27,12 +51,15 @@ function getRandomImage(data) {
     let items = data.collection.items
     let randomItem = (Math.floor(Math.random(items) * 50))
     let chosenItem = items[randomItem]
-    console.log(chosenItem)
+    console.log(chosenItem) // sometimes shows as "undefined" but causes no errors
     renderImageDetails(chosenItem)
-   
 }
 
 function renderImageDetails(chosenItem) {
+    mainTitle.textContent = chosenItem.data[0].title // added by Jandro 12/5/22 evening
     mainImage.src = chosenItem.links[0].href
+    mainExplain.textContent = chosenItem.data[0].description // added by Jandro 12/5/22 evening
+    // sometimes the title and description are the same, maybe we can set it so that if they are the same we only show the title
+
     featuredImageDiv.appendChild(mainImage)
 }
