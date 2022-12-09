4da964cb6ab2f60c062546e1a2bf483a1f36d940
olivia <opierez@gmail.com>
2022-12-09 13:21:13 -0500

    final edits and comments

--- src/index.js
+++ src/index.js
@@ -15,6 +15,7 @@ const pError = document.querySelector('#error-message')
 const imageError = document.querySelector('#error-image')
 const apodButton = document.querySelector('#apod-button')
 const areciboButton = document.querySelector('#arecibo-button')
+const apodDiv = document.querySelector('#apod-div')
 
 // Global variables
 let currentApod = {}
@@ -24,17 +25,17 @@ form.addEventListener('submit', handleSubmit)
 apodButton.addEventListener('click', handleClick)
 
 
-// fetches planet icon images and details from db.json file 
+// Fetches planet icon images and details from db.json file 
 fetch('http://localhost:3000/planets')
     .then(resp => resp.json())
     .then(planets => renderImageIcons(planets))
 
-// fetches arecibo obvservatory image and details
+// Fetches arecibo obvservatory image and details from db.json file
 fetch('http://localhost:3000/observatories')
     .then(resp => resp.json())
     .then(observatories => renderObservatory(observatories))
 
-// renders planet icons to the nav 
+// Renders planet icons to the nav 
 function renderImageIcons(planets) {
     planets.forEach(planet => {
         // console.log(planet)
@@ -46,7 +47,7 @@ function renderImageIcons(planets) {
     })
 }
 
-// when user hovers over planet icon, renders planet details to the "featured image" div
+// When user clicks on planet icon, renders planet image to the "featured image" div and corresponding details to the "column left" div
 function renderImageIconDetails(planet) {
     // console.log(planet)
     mainTitle.textContent = planet.title
@@ -56,8 +57,7 @@ function renderImageIconDetails(planet) {
     mainSource.textContent = "Taken from this Wikipedia source"
 }
 
-// https://api.nasa.gov/ --> scroll down to APOD ('Astronomy Pic Of the Day'), same as HEROKU's. Generates random image of the day
-// fetches astronomy picture of the day 
+// Fetches astronomy picture of the day from NASA APOD API
 fetch('https://api.nasa.gov/planetary/apod?api_key=XjBC4L80dCM7UOyBSupD3sYfCDTWcuAN5LIeb1dv')
     .then(resp => resp.json())
     .then(apod => {
@@ -69,7 +69,7 @@ fetch('https://api.nasa.gov/planetary/apod?api_key=XjBC4L80dCM7UOyBSupD3sYfCDTWc
         currentApod = apod
     })
 
-// renders astronomy picture of the day and corresponding details to the "featured image" div
+// Renders astronomy picture of the day to the "featured image" div and corresponding details to the "column left" div
 function renderAPOD(apod) {
     let apodTitle = apod.title
     let apodImage = apod.url
@@ -80,7 +80,7 @@ function renderAPOD(apod) {
     mainExplain.textContent = apodExplain
 }
 
-// Allows the user to input a search term in the form and then fetch related images from NASA API
+// Handles the form submission based on user search term input and then fetches related images from NASA API
 function handleSubmit(e) {
     e.preventDefault()
     let value = encodeURI(e.target.search_value.value)
@@ -112,32 +112,39 @@ function handleSubmit(e) {
     form.reset()
 }
 
-// renders an image from the fetch or "grid" div in the "featured image" div
+// Renders an image from the search or "grid" div in the "featured image" div. Renders corresponding image details in the "column left" div
 function renderImageDetails(chosenItem) {
+    // clears out any existing error message/image from prior search
     pError.textContent = ""
     imageError.src = ""
     imageError.style.display = "none"
+
+    // variables for image title and image description 
     let chosenItemTitle = chosenItem.data[0].title
     let chosenItemDescription = chosenItem.data[0].description
 
+    // if the image description is the same as the title, remove the description to only show the title, else provide the description
     if (chosenItemTitle === chosenItemDescription) {
         mainExplain.textContent = ''
     } else {
         mainExplain.textContent = chosenItemDescription
     }
-
     mainTitle.textContent = chosenItemTitle
     mainImage.src = chosenItem.links[0].href
+
+    // clears out the source from previous details that were displayed
     mainSource.textContent = ""
     mainSource.href = ""
 
     featuredImageDiv.appendChild(mainImage)
 }
 
-// renders 5 images from the fetch in the "grid" div
+// Renders 5 images from the NASA API fetch in the "grid" div
 function renderGridImages(items) {
+    // clears out grid images from prior search
     grid.innerHTML = ""
-    // Here is where we want to make the display of the card property visible
+
+    // grabs the 20th-25th images from the search result and renders those 5 images in the "grid" div
     renderImageDetails(items[20])
     let slicedItems = items.slice(20, 25)
     slicedItems.forEach(item => {
@@ -147,14 +154,18 @@ function renderGridImages(items) {
         img.src = item.links[0].href
 
         grid.appendChild(img)
-
         img.addEventListener('mouseover', () => handleHover(item))
     })
 }
 
-// When user clicks on "apod-button", return to displaying the apod image and details and hide the grid div  
+// When user clicks on "apod-button", return to displaying the apod image and details   
 function handleClick() {
+    // clears out any existing error message/image from prior search and hides grid display 
+    pError.textContent = ""
+    imageError.src = ""
+    imageError.style.display = "none"
     grid.style.display = "none" 
+
     renderAPOD(currentApod)
 }
 
@@ -165,40 +176,45 @@ function renderObservatory(observatories) {
 
 // When user clicks on "arecibo-button", render arecibo observatory image and details
 function handleAreciboClick(observatories) {
+    // clears out any existing error message/image from prior search and hides grid display 
     pError.textContent = ""
     imageError.src = ""
     imageError.style.display = "none"
     grid.style.display = "none"
+
+    // renders arecibo observatory image and details
     mainTitle.textContent = observatories[0].title
     mainExplain.textContent = observatories[0].description
     mainImage.src = observatories[0].image
     mainSource.textContent = observatories[0].wiki
 }
 
-// When user clicks on an image in the "grid" div, render that image and it's respective details in the "featured image" div
+// When user hovers over an image in the "grid" div, render that image to the "featured image" div and corresponding details to the "column left" div
 function handleHover(item) {
     // console.log(item)
     renderImageDetails(item)
 }
 
-// Shows the user an error if they try to search for something that is unvailable or has a typo 
+// Shows the user an error if they try to search for something that is unavailable or has a typo 
 function renderError() {
     imageError.src = "./assets/images/gotg.gif"
     imageError.style = "display:block"
     // console.log(imageError.src)
     pError.textContent = `Please check your spelling or try another search query.`
+    
+    // removes the title, details, and source from the prior search and hides the grid div
     mainTitle.textContent = ""
     mainExplain.textContent = ""
-    grid.style.display = "none"
     mainSource.textContent = ""
+    grid.style.display = "none"
 }
 
-// forces page to start at the very top on reload or refresh
+// Forces page to start at the very top on reload or refresh
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
   }
 
-// Opens mainImage pic in a new window on double click
+// Opens featured image in a new window on double click
 function swipe() {
     let openImage = document.querySelector("#main_image")
     let url=openImage.getAttribute('src');
